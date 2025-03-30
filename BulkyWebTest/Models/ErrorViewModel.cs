37bbde603dbd559c7fa8fccaf2b19a57abbc263f
Filemon Robert Stevenson Kessing <filemonrsk@gmail.com>
2025-03-30 14:16:33 +0700

    Add project files.

---
+++ BulkyWebTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyWebTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
