6870d64cd6d29687e23260bb9c91085970959234
Filemon Robert Stevenson Kessing <filemonrsk@gmail.com>
2025-04-09 08:38:21 +0700

    SetupDb, Add category

---
+++ BulkyWebTest/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebTest.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
