3a9b38290b750335b3559fcb9efcd24eb0718efc
Filemon Robert Stevenson Kessing <filemonrsk@gmail.com>
2025-04-14 15:21:44 +0700

    Initial Product Page. Migrate Datas

---
+++ BulkyWebTest/Areas/Admin/Controllers/ProductController .cs
@@ -0,0 +1,108 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll()
+                .Select(u => new SelectListItem
+                {
+                    Text = u.CategoryName,
+                    Value = u.Id.ToString()
+                });
+            return View(objProductList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index", "Product"); // View name + controller name
+            }
+
+            return View(); // View name + controller name
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product edited successfully";
+                return RedirectToAction("Index", "Product"); // View name + controller name
+            }
+
+            return View(); // View name + controller name
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product ProductFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product obj = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index"); // View name + controller name
+        }
+    }
+}
